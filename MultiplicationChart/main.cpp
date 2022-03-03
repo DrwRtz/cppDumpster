92d91cccdb37b4f05368e0838d101f2854e0e0e6
Darwin Ortiz <drwn_rtz@outlook.com>
2022-03-03 03:35:27 -0500

    Update

---
+++ MultiplicationChart/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        std::cout << "## TABLA DEL " << i << " ##" << std::endl;
+        for (int j = 1; j <= 10; j++)
+        {
+            int userAttempt;
+            int expectedInput = i * j;
+
+            do {
+                std::cout << i << " X " << j << " = ";
+                
+                std::cin >> userAttempt;
+
+
+                if (userAttempt != expectedInput)
+                {
+                    std::cout << "Vuelve a intentarlo." << std::endl;
+                }
+            } while(userAttempt != expectedInput);
+        }
+    }
+}
